2cac90bcb72c025167b07476163ce57761fd0e21
Phoenix Jauregui <phoenixjauregui@eeace.com>
2018-12-07 12:17:35 -0600

    route fix

--- trump/static/js/static.js
+++ trump/static/js/static.js
@@ -1,4 +1,4 @@
-var twitter_data = d3.json("http://127.0.0.1:5000/approve").then( function(dat) {
+var approve = d3.json("https://trumptweettracker.herokuapp.com/approve").then( function(dat) {
   var approval = []
   var date = []
 
